bf4e388c307791c27735ac9c6694b9b3c3edc492
faya.shz <faya.shz@alibaba-inc.com>
2019-02-19 23:11:28 +0800

    间接赋值的意义

--- src/day02/dm04_ruler3.c
+++ src/day02/dm04_ruler3.c
@@ -13,6 +13,17 @@ void ruler3(int **p) {
     *p = 300;
 }
 
+void init(char **p, int *len) {
+
+    char *temp = (char *) malloc(100);
+    strcpy(temp, "abcdefghij");
+    //二级指针间接赋值
+    *p = temp;
+
+    //一级指针间接赋值
+    *len = strlen(temp);
+}
+
 int main() {
 
     char *p1 = NULL;
@@ -25,4 +36,11 @@ int main() {
 
     ruler3(&p1);
     printf("p1:%d\n", p1);
+
+
+    //间接赋值的意义
+    char *p3 = NULL;
+    int len3;
+    init(&p3, &len3);
+    printf("p3:%s,len:%d\n", p3, len3);
 }
