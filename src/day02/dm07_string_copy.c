792eebf3b2c7ffda4956fc274d7a58a99ecdbba8
faya.shz <faya.shz@alibaba-inc.com>
2019-02-24 13:51:46 +0800

    string copy

--- src/day02/dm07_string_copy.c
+++ src/day02/dm07_string_copy.c
@@ -30,14 +30,29 @@ void stringCopy3(char *from, char *to) {
     }
 }
 
-void stringCopy4(char *from, char *to) {
+/**
+ * 不使用辅助指针变量，很容易改变初始的参数的值。建议使用辅助指针变量；
+ * @param from
+ * @param to
+ */
+void stringCopy4(const char *from, char *to) {
+    printf("stringCopy4 start,from:%s \n", from);
     while ((*to++ = *from++) != '\0') {
     }
+    //from指针变了
+    printf("stringCopy4 end,from:%s \n", from);
 }
 
-void stringCopy5(char *from, char *to) {
-    while ((*to++ = *from++)) {
+int stringCopy5(const char *from, char *to) {
+    if (from == NULL || to == NULL) {
+        return -1;
     }
+    //辅助指针变量,使用辅助指针变量，可以有效的解决入参变化的问题；
+    char *tmpFrom = from;
+    char *tmpTo = to;
+    while ((*tmpTo++ = *tmpFrom++)) {
+    }
+    return 0;
 }
 
 int main() {
@@ -47,8 +62,13 @@ int main() {
 //    stringCopy1(from, buf);
 //    stringCopy2(from, buf);
 //    stringCopy3(from, buf);
-//    stringCopy4(from, buf);
-    stringCopy5(from, buf);
+    stringCopy4(from, buf);
+//    stringCopy5(from, buf);
+//    {
+//        char *to = NULL;
+//        int ret = stringCopy5(from, to);
+//        printf("ret = %d \n", ret);
+//    }
 
     printf("buf:%s \n", buf);
 }
