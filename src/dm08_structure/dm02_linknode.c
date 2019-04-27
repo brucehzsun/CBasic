86997fc2d82060e5a47698edf830c6823f7fc378
faya.shz <faya.shz@alibaba-inc.com>
2019-04-27 15:08:44 +0800

    完成链表的增删改查

--- src/dm08_structure/dm02_linknode.c
+++ src/dm08_structure/dm02_linknode.c
@@ -52,6 +52,26 @@ LIST *createList() {
 }
 
 int insertList(LIST *head, int x, int y) {
+    LIST *pM = (LIST *) malloc(sizeof(LIST));
+    if (pM == NULL) {
+        return -1;
+    }
+    pM->data = y;
+    pM->next = NULL;
+
+    LIST *pPre = head;
+    LIST *pCur = pPre->next;
+
+    while (pCur) {
+        if (pCur->data == x) {
+            pPre->next = pM;
+            pM->next = pCur;
+            break;
+        }
+        pPre = pCur;
+        pCur = pCur->next;
+    }
+
     return 0;
 }
 
@@ -61,18 +81,45 @@ int printList(LIST *head) {
     }
     LIST *pCur = head;
     while (pCur) {
-        printf(">> %d\n", pCur->data);
+        printf("%d ", pCur->data);
+        pCur = pCur->next;
+    }
+
+    printf("\n");
+    return 0;
+}
+
+int deleteList(LIST *head, int y) {
+    if (head == NULL) {
+        return -1;
+    }
+
+    LIST *pPre = head;
+    LIST *pCur = pPre->next;
+    while (pCur) {
+        if (pCur->data == y) {
+            pPre->next = pCur->next;
+            free(pCur);
+            break;
+        }
+        pPre = pCur;
         pCur = pCur->next;
     }
 
     return 0;
 }
 
-int deleteList(LIST *head, int y) {
-    return 0;
-}
-
 int destroyList(LIST *head) {
+    if (head == NULL) {
+        return -1;
+    }
+    LIST *temp = NULL;
+    LIST *pCur = head;
+    while (pCur) {
+        temp = pCur->next;
+        free(pCur);
+        pCur = temp;
+    }
     return 0;
 }
 
@@ -85,4 +132,20 @@ int main() {
     int ret;
     ret = printList(head);
 
+    printf("20前面插入19\n");
+    ret = insertList(head, 20, 19);
+    if (ret != 0) {
+        return ret;
+    }
+    printList(head);
+
+    printf("删除19\n");
+    ret = deleteList(head, 19);
+    if (ret != 0) {
+        return ret;
+    }
+    printList(head);
+
+    printf("销毁链表\n");
+    destroyList(head);
 }
